0349b64d9acfbba0c589aad6a75d4ce9a511603e
GREENJEONG3424\green_jeong <th2gr22n@gmail.com>
2024-05-20 03:18:49 +0900

    [설정 탭] 1. 미량원소 일부 삭제 2. 원수 고려시 입력항목 선택버튼 추가

--- src/main/java/org/main/culturesolutioncalculation/SettingTabController.java
+++ src/main/java/org/main/culturesolutioncalculation/SettingTabController.java
@@ -2,7 +2,6 @@ package org.main.culturesolutioncalculation;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,12 +17,12 @@ public class SettingTabController {
     private RadioButton group2_1, group2_2;
     @FXML
     private RadioButton group3_1, group3_2;
-    @FXML
-    private RadioButton group4_1, group4_2, group4_3;
-    @FXML
-    private RadioButton group5_1, group5_2;
-    @FXML
-    private RadioButton group6_1, group6_2;
+//    @FXML
+//    private RadioButton group4_1, group4_2, group4_3;
+//    @FXML
+//    private RadioButton group5_1, group5_2;
+//    @FXML
+//    private RadioButton group6_1, group6_2;
     @FXML
     private RadioButton group7_1, group7_2;
     @FXML
@@ -34,9 +33,9 @@ public class SettingTabController {
     private ToggleGroup group1 = new ToggleGroup();
     private ToggleGroup group2 = new ToggleGroup();
     private ToggleGroup group3 = new ToggleGroup();
-    private ToggleGroup group4 = new ToggleGroup();
-    private ToggleGroup group5 = new ToggleGroup();
-    private ToggleGroup group6 = new ToggleGroup();
+//    private ToggleGroup group4 = new ToggleGroup();
+//    private ToggleGroup group5 = new ToggleGroup();
+//    private ToggleGroup group6 = new ToggleGroup();
     private ToggleGroup group7 = new ToggleGroup();
     private ToggleGroup group8 = new ToggleGroup();
     private ToggleGroup group9 = new ToggleGroup();
@@ -59,15 +58,15 @@ public class SettingTabController {
         group3_1.setToggleGroup(group3);
         group3_2.setToggleGroup(group3);
 
-        group4_1.setToggleGroup(group4);
-        group4_2.setToggleGroup(group4);
-        group4_3.setToggleGroup(group4);
-
-        group5_1.setToggleGroup(group5);
-        group5_2.setToggleGroup(group5);
-
-        group6_1.setToggleGroup(group6);
-        group6_2.setToggleGroup(group6);
+//        group4_1.setToggleGroup(group4);
+//        group4_2.setToggleGroup(group4);
+//        group4_3.setToggleGroup(group4);
+//
+//        group5_1.setToggleGroup(group5);
+//        group5_2.setToggleGroup(group5);
+//
+//        group6_1.setToggleGroup(group6);
+//        group6_2.setToggleGroup(group6);
 
         group7_1.setToggleGroup(group7);
         group7_2.setToggleGroup(group7);
@@ -86,9 +85,9 @@ public class SettingTabController {
         selectedValues.put("질산칼슘 비료", getSelectedValue(group1));
         selectedValues.put("철 비료", getSelectedValue(group2));
         selectedValues.put("붕소 비료", getSelectedValue(group3));
-        selectedValues.put("망간 비료", getSelectedValue(group4));
-        selectedValues.put("몰리브뎀 비료", getSelectedValue(group5));
-        selectedValues.put("설정 다량원소 단위", getSelectedValue(group6));
+//        selectedValues.put("망간 비료", getSelectedValue(group4));
+//        selectedValues.put("몰리브뎀 비료", getSelectedValue(group5));
+//        selectedValues.put("설정 다량원소 단위", getSelectedValue(group6));
         selectedValues.put("설정 미량원소 단위", getSelectedValue(group7));
         selectedValues.put("원수 입력 단위", getSelectedValue(group9));
 
